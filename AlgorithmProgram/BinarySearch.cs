25b70ee340003da5397ae8f051f26b64e621a612
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-20 21:18:39 +0530

    Added Binary Search Program

---
+++ AlgorithmProgram/BinarySearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    internal class BinarySearch
+    {
+        public void Display()
+        {
+            List<string> list = new List<string>();
+            list.Add("Citrus ");
+            list.Add("Eggplant");
+            list.Add("DragonFruit");
+            list.Add("Apple ");
+            list.Add("Banana");
+
+            Console.WriteLine("The origial list is ");
+            foreach (string g in list)
+            {
+                Console.WriteLine(g);
+            }
+            Console.WriteLine("\n The List is sorted form");
+            list.Sort();
+            Console.WriteLine();
+            foreach (string g in list)
+            {
+                Console.WriteLine(g);
+            }
+            Console.WriteLine("\nInsert Rabbit :");
+
+            int index = list.BinarySearch("Rabbit");
+
+            if (index < 0)
+            {
+                list.Insert(~index, "Rabbit");
+            }
+            Console.WriteLine();
+            foreach (string g in list)
+            {
+                Console.WriteLine(g);
+            }
+            if (index > 0)
+            {
+                Console.WriteLine("Word is found");
+            }
+            else
+            {
+                Console.WriteLine("Word is not found");
+            }
+
+
+        }
+    }
+}
--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AlgorithmPrograms
+﻿using AlgorithmProgram;
+
+namespace AlgorithmPrograms
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Displaying Algorithm Programs");
+            //UC_1
+            BinarySearch binarySearch = new BinarySearch();
+            binarySearch.Display();
         }
     }
 }
