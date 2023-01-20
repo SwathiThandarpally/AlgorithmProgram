9f32986dc9fac693742f238510cb550a4f9969f1
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-20 21:04:51 +0530

    [Swathi]:Added Algorithm Program

---
+++ AlgorithmProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Algorithm Programs");
+        }
+    }
+}
