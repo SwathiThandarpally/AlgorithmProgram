b083f3f885bdcfd59819c2842076f978d45e8a92
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-20 21:51:42 +0530

    Added Bubble Sorting Program

---
+++ AlgorithmProgram/BubbleSorting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    internal class BubbleSorting
+    {
+        public void Display()
+        {
+            int[] arr = { 50,40,30,20,10 };
+            Console.WriteLine("Unsorted Array");
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("sorted array");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p + "");
+            }
+        }
+    }
+}
--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -8,14 +8,17 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine("Displaying Algorithm Programs");
             //UC_1
-            BinarySearch binarySearch = new BinarySearch();
-            binarySearch.Display();
-            Console.WriteLine("------------------------------------");
-            //UC_2
-            int[] array1 = {1,2,3,4,5,6 };
-            InsertionSort sorting = new InsertionSort();
-            sorting.Sorting(array1);
-            sorting.DisplayArray(array1);
+            //BinarySearch binarySearch = new BinarySearch();
+            //binarySearch.Display();
+            //Console.WriteLine("------------------------------------");
+            ////UC_2
+            //int[] array1 = {1,2,3,4,5,6 };
+            //InsertionSort sorting = new InsertionSort();
+            //sorting.Sorting(array1);
+            //sorting.DisplayArray(array1);
+            //UC_3
+            BubbleSorting bubbleSorting = new BubbleSorting();
+            bubbleSorting.Display();
 
         }
     }
